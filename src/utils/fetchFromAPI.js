1734511b797911732d533afba74f41933601f0f7
JaypeeLan <jplaniran01@gmail.com>
2022-12-24 17:59:13 +0100

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "9db1a2a1e3mshaa3962f28e449f7p1ed127jsnf66c08e85dd6",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
